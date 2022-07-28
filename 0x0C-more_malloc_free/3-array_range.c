f1ff264ce51d13e37423ea624839910199536190
somto-ez <adaeze.somy@gmail.com>
2022-07-28 15:11:35 -0700

    3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * array_range
+ * @min: minimum
+ * @max: maximum
+ * Return: array
+ */
+
+int *array_range(int min, int max)
+{
+	int *arr, i = 0, t = min;
+
+	if (min > max)
+		return NULL;
+	arr = malloc((max - min + 1) * sizeof(int));
+
+	if (!arr)
+		return NULL;
+	while (i <= max - min)
+		arr[i++] = t++;
+	return (arr);
+}
