a58eab544eb9fa8487c4ebe0683a84564e3e48e4
somto-ez <adaeze.somy@gmail.com>
2022-08-16 15:06:49 -0700

    msll

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -35,7 +35,3 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-	new_node->next = temp->next;
-	temp->next = new_node;
-	return (new_node);
-}
